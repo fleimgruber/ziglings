a667625c92d878f9c56c9fe30dadb414b6e308d3
Fabian Leimgruber <fabian.leimgruber@ait.ac.at>
2022-06-08 16:36:46 +0200

    mainly comptime

--- exercises/072_comptime7.zig
+++ exercises/072_comptime7.zig
@@ -35,7 +35,9 @@ pub fn main() void {
     // at compile time.
     //
     // Please fix this to loop once per "instruction":
-    ??? (i < instructions.len) : (???) {
+    inline while (i < instructions.len) : (i += 3) {
+
+        // @compileLog("i at compile time: ", i);
 
         // This gets the digit from the "instruction". Can you
         // figure out why we subtract '0' from it?
