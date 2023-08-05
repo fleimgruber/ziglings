79c9e2000a3a47fe0960b37d456f243f06ea08fc
fleimgruber <fabio.leimgruber@posteo.eu>
2023-08-05 14:08:12 +0200

    build: rollback to plain nix

--- default.nix
+++ default.nix
@@ -1,11 +1,5 @@
 let
-  nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/915f5a5b3cc4f8ba206afd0b70e52ba4c6a2796b.tar.gz";
-    sha256 = "06x6h5vdgnkxv70rbi6qmgrfjk9hq9aasi8dgc811m2ipbvq0hba";
-  };
-in
-{ pkgs ? import nixpkgs { } }:
-let
+  pkgs = import <nixpkgs>{};
   pythonOnNix = import (
     builtins.fetchGit {
       ref = "main";
@@ -24,9 +18,10 @@ pkgs.mkShell {
   nativeBuildInputs =
     [ env.dev ]
     ++ (with pkgs; [ cmake gdb ninja qemu zls ])
-    ++ (with pkgs; with llvmPackages_14; [ clang clang-unwrapped lld llvm ]);
+    ++ (with pkgs; with llvmPackages_16; [ clang clang-unwrapped lld llvm ]);
   hardeningDisable = [ "all" ];
   shellHook = ''
     export PATH=/home/fps/dev/zig-bootstrap/out/zig-x86_64-linux-musl-native/bin:$PATH
+    export PATH=/home/fps/dev/zig/build/stage3/bin:$PATH
   '';
 }
